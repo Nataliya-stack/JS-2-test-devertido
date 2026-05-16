fd4718cc1b798238033287f67acb36f99dce0a72
Natalia <nataliya.yanovic@mail.ru>
2026-05-17 00:28:47 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,75 @@
+const myButton = document.querySelector("#myButton");
+console.log(myButton);
+const PRIMERO = document.querySelector("#uno")
+const VACA = document.querySelector("#vaca");
+console.log(VACA);
+const TIGRE = document.querySelector("#tigre");
+const OVEJA = document.querySelector("#obeja");
+const CABALLO = document.querySelector("#caballo");
+const MONO = document.querySelector("#mono");
+const SEGUNDO = document.querySelector("#dos")
+const PERRO = document.querySelector("#perro");
+const GATO = document.querySelector("#gato");
+const RATA = document.querySelector("#rata");
+const CAFE = document.querySelector("#cafe");
+const MAR = document.querySelector("#mar");
+const TERSERA = document.querySelector("#tres")
+const AMARILLO = document.querySelector("#amarillo");
+const NARANJA = document.querySelector("#naranja");
+const BLANCO = document.querySelector("#blanco");
+const ROJO = document.querySelector("#rojo");
+const VERDE = document.querySelector("#verde");
+
+function calcular() {
+    PRIMERO.textContent = "Tu orden de prioridades:"
+    PRIMERO.style.fontWeight = "bold";
+    VACA.textContent = "Tu Carrera";
+    TIGRE.textContent = "El orgullo";
+    OVEJA.textContent = "El amor";
+    CABALLO.textContent = "La familia";
+    MONO.textContent = "El dinero";
+    SEGUNDO.textContent = "Como ves la vida:"
+    SEGUNDO.style.fontWeight = "bold";
+    PERRO.textContent = "Yo Soy...";
+    GATO.textContent = "Mi Pareja,";
+    RATA.textContent = "Lo que mas odio";
+    CAFE.textContent = "Para mi el sexo es";
+    MAR.textContent = "Y la vida...";
+    TERSERA.textContent = "Tu gente:"
+    TERSERA.style.fontWeight = "bold";
+    AMARILLO.textContent = "Nunca te olvidara";
+    NARANJA.textContent = "Puede ser tu gran amigo/a";
+    BLANCO.textContent = "Tu Alma Gemela:";
+    ROJO.textContent = "Realmente amas a...";
+    VERDE.textContent = "Siempre estara en tu memoria";
+}
+
+//myButton.addEventListener("click", () => {
+    //PRIMERO.textContent = "Tu orden de prioridades:"
+    //PRIMERO.style.fontWeight = "bold";
+    //VACA.textContent = "Tu Carrera";
+    //TIGRE.textContent = "El orgullo";
+    //OVEJA.textContent = "El amor";
+    //CABALLO.textContent = "La familia";
+    //MONO.textContent = "El dinero";
+    //SEGUNDO.textContent = "Como ves la vida:"
+    //SEGUNDO.style.fontWeight = "bold";
+    //PERRO.textContent = "Yo Soy...";
+    //GATO.textContent = "Mi Pareja,";
+    //RATA.textContent = "Lo que mas odio";
+    //CAFE.textContent = "Para mi el sexo es";
+    //MAR.textContent = "Y la vida...";
+    //TERSERA.textContent = "Tu gente:"
+    //TERSERA.style.fontWeight = "bold";
+    //AMARILLO.textContent = "Nunca te olvidara";
+    //NARANJA.textContent = "Puede ser tu gran amigo/a";
+    //BLANCO.textContent = "Tu Alma Gemela:";
+    //ROJO.textContent = "Realmente amas a...";
+    //VERDE.textContent = "Siempre estara en tu memoria";       
+//})
+
+
+
+
+
+
